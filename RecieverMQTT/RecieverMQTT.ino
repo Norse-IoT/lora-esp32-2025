e8bbf7c0febbd63c9c0ef181bf79c3799a74e585
Zack Sargent <zack@zack.fyi>
2025-04-02 19:44:12 -0400

    it works!

--- RecieverMQTT/RecieverMQTT.ino
+++ RecieverMQTT/RecieverMQTT.ino
@@ -1,93 +1,80 @@
-#include <Arduino.h>
-#include <RadioLib.h>  // RadioLib by Jan Gromes v7.1.2
-#include <ArduinoJson.h>
+/**
+ * Send and receive LoRa-modulation packets with a sequence number, showing RSSI
+ * and SNR for received packets on the little display.
+ *
+ * Note that while this send and received using LoRa modulation, it does not do
+ * LoRaWAN. For that, see the LoRaWAN_TTN example.
+ *
+ * This works on the stick, but the output on the screen gets cut off.
+*/
+
+
 
 // Turns the 'PRG' button into the power button, long press is off
 #define HELTEC_POWER_BUTTON  // must be before "#include <heltec_unofficial.h>"
+#include <heltec_unofficial.h>
 
-// Uncomment this if you have Wireless Stick v3
-// #define HELTEC_WIRELESS_STICK
+// Frequency in MHz. Keep the decimal point to designate float.
+// Check your own rules and regulations to see what is legal where you are.
+#define FREQUENCY 866.3  // for Europe
+// #define FREQUENCY           905.2       // for US
 
-
-#define FREQUENCY 905.2  // for US
+// LoRa bandwidth. Keep the decimal point to designate float.
+// Allowed values are 7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125.0, 250.0 and 500.0 kHz.
 #define BANDWIDTH 250.0
 
-// creates 'radio', 'display' and 'button' instances
-#include <heltec_unofficial.h>  // Heltec_ESP32_LoRa_v3 by Rop Gonggrijp v0.9.2
+// Number from 5 to 12. Higher means slower but higher "processor gain",
+// meaning (in nutshell) longer range and more robust against interference.
+#define SPREADING_FACTOR 9
 
+// Transmit power in dBm. 0 dBm = 1 mW, enough for tabletop-testing. This value can be
+// set anywhere between -9 dBm (0.125 mW) to 22 dBm (158 mW). Note that the maximum ERP
+// (which is what your antenna maximally radiates) on the EU ISM band is 25 mW, and that
+// transmissting without an antenna can damage your hardware.
+#define TRANSMIT_POWER 0
+
+String rxdata;
 volatile bool rxFlag = false;
-// Callback Can't do Serial or display things here, takes too much time for the interrupt
+long counter = 0;
+uint64_t last_tx = 0;
+uint64_t tx_time;
+uint64_t minimum_pause;
+
 void rx() {
   rxFlag = true;
 }
 
 void setup() {
   heltec_setup();
-  Serial.begin(9600);
+  both.println("Radio init");
   RADIOLIB_OR_HALT(radio.begin());
-
+  // Set the callback function for received packets
+  radio.setDio1Action(rx);
+  // Set radio parameters
   both.printf("Frequency: %.2f MHz\n", FREQUENCY);
   RADIOLIB_OR_HALT(radio.setFrequency(FREQUENCY));
   both.printf("Bandwidth: %.1f kHz\n", BANDWIDTH);
   RADIOLIB_OR_HALT(radio.setBandwidth(BANDWIDTH));
-
-  display.println("Receiver Init");
-  delay(2000);
-
-  // Initialize Radio
-  display.print("Radio ");
-  int state = radio.begin();
-  if (state == RADIOLIB_ERR_NONE) {
-    display.println("OK");
-  } else {
-    display.printf("fail, code: %i\n", state);
-    while (true) { delay(10); }  // Halt if initialization fails
-  }
-
-  // controller.setFrequency(915.0);
-  radio.setDio1Action(rx);
+  both.printf("Spreading Factor: %i\n", SPREADING_FACTOR);
+  RADIOLIB_OR_HALT(radio.setSpreadingFactor(SPREADING_FACTOR));
+  both.printf("TX power: %i dBm\n", TRANSMIT_POWER);
+  RADIOLIB_OR_HALT(radio.setOutputPower(TRANSMIT_POWER));
+  // Start receiving
   RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
 }
 
 void loop() {
   heltec_loop();
-  Serial.println("[SX1262] Waiting for incoming transmission...");
 
+  // If a packet was received, display it and the RSSI and SNR
   if (rxFlag) {
-    display.println("Receiving...");
-    String receivedData;
-    radio.readData(receivedData);
-
+    rxFlag = false;
+    radio.readData(rxdata);
     if (_radiolib_status == RADIOLIB_ERR_NONE) {
-      Serial.println("Received Data: " + receivedData);
-      display.clear();
-      display.println("Received Data:");
-      display.println(receivedData);
-      display.display();
-
-      // Parse JSON data
-      DynamicJsonDocument doc(1024);
-      DeserializationError error = deserializeJson(doc, receivedData);
-      if (!error) {
-        float tempC = doc["Temp"]["DegreesC"];
-        float tempF = doc["Temp"]["DegreesF"];
-        float humidity = doc["Humidity"];
-
-        Serial.printf("Temp: %.1fC (%.1fF), Humidity: %.1f%%\n", tempC, tempF, humidity);
-        display.clear();
-        display.printf("Temp: %.1fC\nHum: %.1f%%\n", tempC, humidity);
-        display.display();
-      } else {
-        Serial.println("JSON Parse Failed!");
-      }
-    } else if (_radiolib_status == RADIOLIB_ERR_RX_TIMEOUT) {
-      Serial.println("Receive Timeout");
-    } else if (_radiolib_status == RADIOLIB_ERR_CRC_MISMATCH) {
-      Serial.println("CRC Error");
-    } else {
-      Serial.printf("Receive Failed, Code: %i\n", _radiolib_status);
+      both.printf("RX [%s]\n", rxdata.c_str());
+      both.printf("  RSSI: %.2f dBm\n", radio.getRSSI());
+      both.printf("  SNR: %.2f dB\n", radio.getSNR());
     }
+    RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
   }
-
-  RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
 }
